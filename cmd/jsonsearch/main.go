719d995c4c745c8e575cd375f3fd2bd2b0fdb9c7
linuxmonk <monklinux5@gmail.com>
2021-03-05 16:12:01 +1100

    Update command line flags
    * Add flags to accept search parameters
      keypath, value and dbname.
    * Add call to search
    * Add stub display function

--- cmd/jsonsearch/main.go
+++ cmd/jsonsearch/main.go
@@ -15,6 +15,9 @@ func main() {
 	var dbfiles DBFiles
 	var indexKeys IndexBy
 	var keyRelns KeyRelations
+	var dbname string
+	var keyPath string
+	var value string
 
 	flag.Var(&dbfiles, "dbfiles", "Comma separated list of filenames/filepaths")
 	flag.Var(
@@ -25,9 +28,17 @@ func main() {
 	flag.Var(&keyRelns, "relationships", "Comma separated list of relationships\n"+
 		"with each relationship delimited with a colon."+
 		"\nExample: organizations._id:tickets.organization_id,users.organization_id:organizations._id")
+	flag.StringVar(&keyPath, "keypath", "", "Dot separated path to the JSON key")
+	flag.StringVar(&dbname, "searchdb", "", "Name of database to search")
+	flag.StringVar(&value, "searchvalue", "", "Search value")
 	flag.Parse()
 	if dbfiles == nil || len(dbfiles) == 0 {
 		fmt.Println("Missing required argument: -dbfiles")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if strings.TrimSpace(dbname) == "" || strings.TrimSpace(keyPath) == "" || strings.TrimSpace(value) == "" {
+		fmt.Println("Missing required argument(s): -keypath / -searchvalue")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -43,6 +54,42 @@ func main() {
 		err = jsonDb.Index(dbname, jsonkey, true)
 		if err != nil {
 			log.Println("Indexing has failed. This will make searches slow")
+		}
+	}
+	fmt.Println("================ Test Data Search Array/List values =================")
+	results, err := jsonDb.Search(dbname, keyPath, value)
+	if err != nil {
+		fmt.Println(err)
+	}
+	PrintResults(results)
+}
+
+func PrintResults(results []interface{}) {
+	if len(results) == 0 {
+		fmt.Println("Empty results")
+	}
+	fmt.Println("----------------------------------------------------")
+	for i, r := range results {
+		fmt.Println("----")
+		dict, ok := r.(map[string]interface{})
+		if !ok {
+			fmt.Printf("Unrecognized format for result at index %d\n", i)
+		}
+		for k, v := range dict {
+			switch val := v.(type) {
+			case float64:
+				fmt.Printf("%s: %v\n", k, val)
+			case int:
+				fmt.Printf("%s: %v\n", k, val)
+			case bool:
+				fmt.Printf("%s: %v\n", k, val)
+			case string:
+				fmt.Printf("%s: %s\n", k, val)
+			case []interface{}:
+				fmt.Printf("%s: %v\n", k, val)
+			default:
+				fmt.Printf("Unknown Type: %T. Key = %s, Val = %v\n", val, k, val)
+			}
 		}
 	}
 }
