dd7912e51b2f1c5beb5acabe14dcef8f2d79741e
linuxmonk <monklinux5@gmail.com>
2021-03-05 12:44:17 +1100

    Add benchmark tests
    * Add large 24 MB JSON test file
    * Update unit tests and benchmark tests for search operations

--- internal/pkg/db/db_test.go
+++ internal/pkg/db/db_test.go
@@ -19,6 +19,15 @@ func TestLoadSuccess(t *testing.T) {
 	assert.Equal(t, len(jsonDb.dbMap), len(files))
 }
 
+func TestLoadLargeDatabase(t *testing.T) {
+	files := []string{
+		"./testdata/24mb.json",
+	}
+	jsonDb, err := Load(files)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(jsonDb.dbMap), len(files))
+}
+
 func TestLoadFail(t *testing.T) {
 	files := []string{
 		"./testdata/doesnotexist.json",
@@ -42,6 +51,17 @@ func TestIndexDBSuccess(t *testing.T) {
 	assert.Equal(t, err, nil)
 }
 
+func TestIndexLargeDB(t *testing.T) {
+	files := []string{
+		"./testdata/24mb.json",
+	}
+	jsonDb, err := Load(files)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(jsonDb.dbMap), len(files))
+	err = jsonDb.Index("24mb", "id", true)
+	assert.Equal(t, err, nil)
+}
+
 func TestIndexDBFailWrongKey(t *testing.T) {
 	files := []string{
 		"./testdata/organizations.json",
@@ -52,6 +72,72 @@ func TestIndexDBFailWrongKey(t *testing.T) {
 	assert.Equal(t, err, nil)
 	err = jsonDb.Index("organizations", "nokey", true)
 	assert.Equal(t, err, ErrKeyNotFound)
+}
+
+func TestLargeDBTest(t *testing.T) {
+	files := []string{
+		"./testdata/24mb.json",
+	}
+	jsonDb, err := Load(files)
+	assert.Equal(t, err, nil)
+	err = jsonDb.Index("24mb", "id", true)
+	assert.Equal(t, err, nil)
+
+	tests := []struct {
+		name           string
+		dbname         string
+		key            string
+		value          string
+		err            error
+		returnValCount int
+	}{
+		{
+			"Search for a value that exists in the data set",
+			"24mb",
+			"id",
+			"2489651045",
+			nil,
+			1,
+		},
+		{
+			"Search for a value that does not exist in the data set",
+			"24mb",
+			"id",
+			"1",
+			ErrKeyValueNotFound,
+			0,
+		},
+		{
+			"Search for with a non existent key",
+			"24mb",
+			"nokey",
+			"101",
+			ErrKeyValueNotFound,
+			0,
+		},
+		{
+			"Search with a non existent dbname",
+			"wrongobject",
+			"_id",
+			"101",
+			ErrInvalidDatabase,
+			0,
+		},
+		{
+			"Search for values embedded in lists",
+			"24mb",
+			"sha",
+			"6b089eb4a43f728f0a594388092f480f2ecacfcd",
+			nil,
+			1,
+		},
+	}
+	for _, test := range tests {
+		log.Println("Test: ", test.name)
+		_, err := jsonDb.Search(test.dbname, test.key, test.value)
+		assert.Equal(t, err, test.err)
+	}
+
 }
 
 func TestIndexedSearch(t *testing.T) {
@@ -119,4 +205,84 @@ func TestIndexedSearch(t *testing.T) {
 		_, err := jsonDb.Search(test.dbname, test.key, test.value)
 		assert.Equal(t, err, test.err)
 	}
+}
+
+// initialize the JsonDB into package level variables to eliminate the
+// loading from the search operations during the benchmark tests.
+
+var benchlargeDb *JsonDB
+var benchJsonDb *JsonDB
+var benchResult []interface{}
+var dbInitDone bool
+var largeDbInitDone bool
+
+func dbinit() {
+	if dbInitDone {
+		return
+	}
+	files := []string{
+		"./testdata/organizations.json",
+		"./testdata/tickets.json",
+		"./testdata/users.json",
+	}
+	jdb, _ := Load(files)
+	jdb.Index("organizations", "_id", true)
+	benchJsonDb = &jdb
+	dbInitDone = true
+}
+
+func largeDbInit() {
+	if largeDbInitDone {
+		return
+	}
+	files := []string{
+		"./testdata/24mb.json",
+	}
+	jdb, _ := Load(files)
+	jdb.Index("24mb", "id", true)
+	benchlargeDb = &jdb
+	largeDbInitDone = true
+}
+
+func TestMain(m *testing.M) {
+	log.Println("Setup for benchmark tests")
+	dbinit()
+	largeDbInit()
+	m.Run()
+}
+
+func benchmarkSearch(jsonDb *JsonDB, dbname, key, value string, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		benchResult, _ = jsonDb.Search(dbname, key, value)
+	}
+}
+
+func BenchmarkIndexedSearchByKey(b *testing.B) {
+	benchmarkSearch(benchJsonDb, "organizations", "_id", "105", b)
+}
+func BenchmarkIndexedSearchValueNotFound(b *testing.B) {
+	benchmarkSearch(benchJsonDb, "organizations", "_id", "999999999", b)
+}
+func BenchmarkSearchByKey(b *testing.B) {
+	benchmarkSearch(benchJsonDb, "organizations", "name", "Enthaze", b)
+}
+func BenchmarkSearchByKeyNoValue(b *testing.B) {
+	benchmarkSearch(benchJsonDb, "organizations", "name", "zzz", b)
+}
+func BenchmarkSearchListValues(b *testing.B) {
+	benchmarkSearch(benchJsonDb, "organizations", "tags", "Frank", b)
+}
+
+// Large DB benchmark tests
+func BenchmarkLargeDBIndexedSearchByKey(b *testing.B) {
+	benchmarkSearch(benchlargeDb, "24mb", "id", "2489651051", b)
+}
+func BenchmarkLargeDBIndexedSearchValueNotFound(b *testing.B) {
+	benchmarkSearch(benchlargeDb, "24mb", "id", "999999999", b)
+}
+func BenchmarkLargeDBSearchByKey(b *testing.B) {
+	benchmarkSearch(benchlargeDb, "24mb", "login", "rspt", b)
+}
+func BenchmarkLargeDBSearchByKeyNoValue(b *testing.B) {
+	benchmarkSearch(benchlargeDb, "24mb", "login", "zzz", b)
 }
